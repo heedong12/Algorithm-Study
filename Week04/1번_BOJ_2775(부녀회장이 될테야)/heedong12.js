6c0185fdc64d8f0d78bf3eda0a445f0486935735
Lee_HeeYeon <114553010+heedong12@users.noreply.github.com>
2025-03-03 17:40:01 +0900

    [BOJ] / B1 / 96ms / 이희연

---
+++ Week04/1번_BOJ_2775(부녀회장이 될테야)/heedong12.js
@@ -0,0 +1,26 @@
+const [T, ...inputs] = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+
+const dp = Array.from({ length: 15 }, () => new Array(15));
+const answer = [];
+
+for (let j = 0; j < dp.length; j++) {
+  for (let k = 0; k < dp[0].length; k++) {
+    if (k <= 1) dp[j][k] = 1;
+    else if (j === 0) dp[j][k] = k;
+    else dp[j][k] = dp[j - 1][k] + dp[j][k - 1];
+  }
+}
+
+for (let i = 0; i < inputs.length; i = i + 2) {
+  let K = inputs[i];
+  let N = inputs[i + 1];
+
+  answer.push(dp[K][N]);
+}
+
+console.log(answer.join("\n"));
