0d6c29458eafadd2981ab46a034a4fe0f735c6c1
zkaakakg <cscom222@naver.com>
2025-03-10 21:25:23 +0900

    [BOJ] / S5 / 1052ms / 장가은

---
+++ Week05/1번_BOJ_4158(CD)/zkaakakg.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringBuilder sb = new StringBuilder();
+        
+		while(true) {
+			String[] input = br.readLine().split(" ");
+			long n = Long.parseLong(input[0]);
+			long m = Long.parseLong(input[1]);
+			
+			if(n == 0 && m == 0) break;
+			
+			HashSet<Long> set = new HashSet<>();
+			for(int i = 0; i < n ; i ++) {
+				set.add(Long.parseLong(br.readLine()));
+			}
+            
+			int cnt = 0;
+			for(int i = 0; i < m; i++) {
+				long temp = Long.parseLong(br.readLine());
+				if(set.contains(temp)) {
+					cnt++;
+				}
+			}
+      sb.append(cnt).append("\n");
+		}
+		System.out.print(sb.toString());
+
+	}
+
+}
