22a4f332599eb0ccde006f434680cf49291a01a7
Lee_HeeYeon <114553010+heedong12@users.noreply.github.com>
2025-03-11 22:02:02 +0900

    [BOJ] / S3 / 240ms / 이희연

---
+++ Week05/2번_BOJ_7795(먹을 것인가 먹힐 것인가)/heedong12.js
@@ -0,0 +1,34 @@
+const [T, ...inputs] = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const answer = [];
+for (let i = 0; i < inputs.length; i = i + 3) {
+  let [N, M] = inputs[i].split(" ").map(Number);
+  let A = inputs[i + 1].split(" ").map(Number);
+  let B = inputs[i + 2]
+    .split(" ")
+    .map(Number)
+    .sort((a, b) => a - b);
+
+  let count = 0;
+  A.forEach((item) => {
+    let start = 0;
+    let end = M - 1;
+
+    while (start <= end) {
+      let mid = Math.floor((start + end) / 2);
+      if (B[mid] < item) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+    count += start;
+  });
+  answer.push(count);
+}
+
+console.log(answer.join("\n"));
