c383521e9ce2e2da8a430dbefed77141cd26c7f3
ooinl77 <115407275+ooinl77@users.noreply.github.com>
2025-03-15 00:04:09 +0900

    [BOJ] / S3 / 112ms / 류형선

--- Week05/3번_BOJ_1166(선물)/ooinl77.java
+++ Week05/3번_BOJ_1166(선물)/ooinl77.java
@@ -4,29 +4,24 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+        int H = Integer.parseInt(st.nextToken());
 
-        while (true) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken()); // 상근이 CD 
-            int M = Integer.parseInt(st.nextToken()); // 선영이 CD 
+        double left = 0; //최소
+        double right = Math.min(L, Math.min(W, H)); //최대
 
-            if (N == 0 && M == 0) break; 
-
-            HashSet<Integer> CD = new HashSet<>();
-            int count = 0;
-
-            for (int i = 0; i < N; i++) {
-                CD.add(Integer.parseInt(br.readLine()));
+        for(int i = 0; i < 10000; i++){
+            double mid = (left + right) / 2;
+            long  count = ((long) (L / mid)) * ((long) (W / mid)) * ((long) (H / mid));
+            if ( count >=N ) {
+                left = mid;  // 가능하면 더 큰 정육면체를 시도
+            } else {
+                right = mid; // 불가능하면 더 작은 정육면체를 시도
             }
-
-            for (int i = 0; i < M; i++) {
-                if (CD.contains(Integer.parseInt(br.readLine()))) {
-                    count++;
-                }
-            }
-
-            System.out.println(count);
         }
+        System.out.printf("%.10f\n", left);
     }
 }
