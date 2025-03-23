44c543bc1d1148480bedf55bb498c20646420d50
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-03-23 21:37:06 +0900

    [BOJ] / G4 / 3992ms, 220ms / 김경민

--- Week06/4번_BOJ_1062(가르침)/rvbear.js
+++ Week06/4번_BOJ_1062(가르침)/rvbear.js
@@ -1,3 +1,4 @@
+// Ver 1
 const input = require("fs")
   .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
   .toString()
@@ -29,6 +30,48 @@ const backtracking = (alphabet, len) => {
       visit[i] = true;
       backtracking(i + 1, len + 1);
       visit[i] = false;
+    }
+  }
+};
+
+backtracking(0, 5);
+
+console.log(k < 5 ? 0 : k === 26 ? n : max);
+
+// Ver 2
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [n, k] = input.shift().split(" ").map(Number);
+let words = input.map((line) => {
+  let mask = 0;
+  for (const char of line.trim().slice(4, -4)) {
+    mask |= 1 << (char.charCodeAt(0) - 97);
+  }
+  return mask;
+});
+
+let visit = 0;
+[0, 2, 8, 13, 19].forEach((c) => (visit |= 1 << c));
+let max = 0;
+
+const countReadableWords = () =>
+  words.reduce((cnt, word) => ((word & visit) === word ? cnt + 1 : cnt), 0);
+
+const backtracking = (alphabet, len) => {
+  if (len === k) {
+    max = Math.max(max, countReadableWords());
+    return;
+  }
+
+  for (let i = alphabet; i < 26; i++) {
+    if (!(visit & (1 << i))) {
+      visit |= 1 << i;
+      backtracking(i + 1, len + 1);
+      visit &= ~(1 << i);
     }
   }
 };
