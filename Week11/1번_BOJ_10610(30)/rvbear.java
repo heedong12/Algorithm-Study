d0d19aecc44d7076108d3e5921d894ac6612eb74
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-04-22 10:48:38 +0900

    [BOJ] / S4 / 156ms, 528ms / 김경민

--- Week11/1번_BOJ_10610(30)/rvbear.java
+++ Week11/1번_BOJ_10610(30)/rvbear.java
@@ -43,8 +43,7 @@ import java.util.stream.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder stb = new StringBuilder(br.readLine().chars().sorted().mapToObj(c -> String.valueOf((char) c)).collect(Collectors.joining())).reverse();
+        StringBuilder stb = new StringBuilder(new BufferedReader(new InputStreamReader(System.in)).readLine().chars().sorted().mapToObj(c -> String.valueOf((char) c)).collect(Collectors.joining())).reverse();
         System.out.println(new BigInteger(stb.toString()).mod(BigInteger.valueOf(30)).equals(BigInteger.ZERO) ? stb : -1);
     }
 }
